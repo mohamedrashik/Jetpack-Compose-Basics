4471987215fcbb3d64da990d1242e8a7064546de
Mohamed Rashik <mohamed.rashik@panasonic.aero>
2022-08-24 15:05:30 +0530

    jetpack basic

---
+++ app/src/main/java/com/droid/jetpackcompose/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.droid.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.droid.jetpackcompose.ui.theme.JetPackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = getString(R.string.app_name))
+        }
+    }
+}
